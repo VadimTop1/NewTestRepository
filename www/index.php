86e8fd5ace94daf680465f6148243399ec370b08
Vadim <vadim_1423@mail.ru>
2021-02-04 13:52:19 +0300

    Добавляю коммит из vs code

---
+++ www/index.php
@@ -0,0 +1,3 @@
+<?php
+
+include_once "/openserver/domains/test.loc/hello.html";
---
+++ www/js/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+class Test {
+    constructor() {
+
+    }
+
+    Name(params) {
+        return (params) => `Hello, your name: ${param}`;
+    }
+}
+
+let testDiv = `
+    <div class="${asd}">
+
+    </div>
+`
+
+let test = new Test();
+alert(test.Name("Holden"));
